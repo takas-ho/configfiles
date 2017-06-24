808d94817548d18b1431ba92c122e50d715f837b
HOMMA Takashi <hommatks@gmail.com>
2017-06-25 07:35:55 +0900

    スペース区切りのパスに対応するためにカンマ区切り.VSCode追加

--- manage.sh
+++ manage.sh
@@ -35,11 +35,11 @@ function unlink_geo () {
 
 # Loops through and link all files without links
 function install_links () {
-    for FOLDER in "${FOLDERS[@]}"
+    for FOLDER in ${FOLDERS[@]}
     do
         link $FOLDER
     done
-    for FILE in "${FILES[@]}"
+    for FILE in ${FILES[@]}
     do
         link $FILE
     done
@@ -47,11 +47,11 @@ function install_links () {
 
 # Function to remove all linked files
 function remove_links () {
-    for FOLDER in "${FOLDERS[@]}"
+    for FOLDER in ${FOLDERS[@]}
     do
         unlink_geo $FOLDER
     done
-    for FILE in "${FILES[@]}"
+    for FILE in ${FILES[@]}
     do
         unlink_geo $FILE
     done
@@ -69,20 +69,26 @@ if [[ $# != 1 ]]; then
 fi
 
 if [ "$(uname)" == "Darwin" ]; then
-    FOLDERS=("mac/Library/Preferences/muCommander")
+    FOLDERS=("mac/Library/Preferences/muCommander","mac/Library/Application Support/Code/User")
     FILES=()
 elif [ "$(uname)" == "Linux" ]; then
     FOLDERS=("linux/.mucommander")
-    FILES=("linux/.config/Code/User/keybindings.json" "linux/.config/Code/User/settings.json" "linux/.config/krusaderrc")
+    FILES=("linux/.config/Code/User/keybindings.json","linux/.config/Code/User/settings.json","linux/.config/krusaderrc")
 fi
 
 # Check whether the user is installing or removing
 if [[ $1 == "install" ]]; then
+    IFS_bak=$IFS
+    IFS=','
     install_links
+    IFS=$IFS_bak
     ## It's required for this to have these permissions
     #chmod 0600 ~/.mutt/msmtprc
 elif [[ $1 == "remove" ]]; then
+    IFS_bak=$IFS
+    IFS=','
     remove_links
+    IFS=$IFS_bak
 else
     die
 fi
